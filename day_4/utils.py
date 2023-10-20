776f50c9aa6851dde46bd2417fdb62717577e4f6
Connor-Prime <connorprime23@gmail.com>
2023-10-19 21:48:41 -0700

    added day 4 homework

---
+++ day_4/utils.py
@@ -0,0 +1,16 @@
+from math import pi
+
+
+def get_house_area(length, width):
+    '''
+    get_area takes in length and width as parameters
+    
+    returns the area of a house with that length and width
+    '''
+    return length * width
+
+def get_cirle_circumference(radius):
+    '''
+    returns the area of a circle with a given radius
+    '''
+    return radius * 2 * pi
